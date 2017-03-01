330c329be5e57ee4c8cf39b33e5ee9b2306c6d37
Amanda <agelmore@umich.edu>
2017-03-01 09:27:26 -0500

    all changes for final analysis

--- analysis_driver.bash
+++ analysis_driver.bash
@@ -12,7 +12,7 @@ rm StabilityNoMetaG.tar
 # clean up the directories to remove the extra files
 curl -LO http://mothur.org/w/images/1/15/Silva.seed_v123.tgz
 tar xvzf Silva.seed_v123.tgz silva.seed_v123.align silva.seed_v123.tax
-mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
+code/mothur/mothur "#get.lineage(fasta=silva.seed_v123.align, taxonomy=silva.seed_v123.tax, taxon=Bacteria);degap.seqs(fasta=silva.seed_v123.pick.align, processors=8)"
 mv silva.seed_v123.pick.align data/references/silva.seed.align
 rm Silva.seed_v123.tgz silva.seed_v123.*
 rm mothur.*.logfile
@@ -26,19 +26,19 @@ rmdir trainset14_032015.pds
 rm Trainset14_032015.pds.tgz
 
 # Generate a customized version of the SILVA reference database that targets the V4 region
-mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
+code/mothur/mothur "#pcr.seqs(fasta=data/references/silva.seed.align, start=11894, end=25319, keepdots=F, processors=8)"
 mv data/references/silva.seed.pcr.align data/references/silva.v4.align
 
 
-# Run mothur through the various quality control steps
-mothur code/get_good_seqs.batch
+# Run mothur through the various quality control steps and generate fully curated sequences
+code/mothur/mothur code/get_good_seqs.batch
 
 
 # Run mock community data through mothur to calculate the sequencing error rates
-mothur code/get_error.batch
+code/mothur/mothur code/get_error.batch
 
-
-mothur code/get_shared_otus.batch
+#Generate the shared files
+code/mothur/mothur code/get_shared_otus.batch
 
 
 
